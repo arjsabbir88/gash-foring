f35b038efb3c431e870bca6609c46c3bcd5ace94
Tariqul Islam Khan <148646871+arjsabbir88@users.noreply.github.com>
2026-03-18 11:24:04 +0600

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,11 @@ import Navbar from "../components/navbar";
 
 export default async function DashboardPage() {
 // https://gash-foring-server.vercel.app
-  const res = await fetch("https://gash-foring-server.vercel.app/api/bookings")
+  //const res = await fetch("https://gash-foring-server.vercel.app/api/bookings")
+
+  const res = await fetch("https://gash-foring-server.vercel.app/api/bookings", {
+  cache: "no-store",
+})
   const data = await res.json()
 
   return (
